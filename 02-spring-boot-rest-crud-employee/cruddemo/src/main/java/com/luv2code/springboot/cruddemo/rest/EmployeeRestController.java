9db1fe5ce42e91cdb5aff6622f5427d4ff9c0106
Bryan Mirtil <166929786+Bryan2397@users.noreply.github.com>
2026-01-08 18:28:16 -0500

    Implement CRUD operations for Employee entity
    Added CRUD operations for Employee entity including get, add, update, patch, and delete functionalities.

--- 02-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ 02-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -25,4 +25,65 @@ public class EmployeeRestController {
         return employeeService.findAll();
     }
 
+    // get employee by id
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployeeId(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if(theEmployee == null){
+            throw new RuntimeException("Employee does not exist");
+        }
+        return theEmployee;
+    }
+
+    // add mapping for adding new employee
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+        theEmployee.setId(0);
+
+        Employee savedEmployee = employeeService.save(theEmployee);
+
+        return savedEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee){
+        Employee saveEmployee = employeeService.save(theEmployee);
+
+        return saveEmployee;
+    }
+
+    // add mapping for PATCH
+    @PatchMapping("/employees/{employeeId}")
+    public Employee patchEmployee(@PathVariable int employeeId, @RequestBody Map<String, Object> patchPayload){
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        if(tempEmployee == null){
+            throw new RuntimeException("Employee id not found");
+        }
+
+        if(patchPayload.containsKey("id")){
+            throw new RuntimeException("Employee id not allowed in request body - " + employeeId);
+        }
+
+        Employee patchedEmployee = jsonMapper.updateValue(tempEmployee, patchPayload);
+
+        Employee saveEmployee = employeeService.save(patchedEmployee);
+        return saveEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId){
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        if(tempEmployee == null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
+    }
 }
+
