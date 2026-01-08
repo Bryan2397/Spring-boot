460af0aaed016e282a51589173bb4c05eb63c8e1
Bryan Mirtil <166929786+Bryan2397@users.noreply.github.com>
2026-01-08 18:03:37 -0500

    Add methods to EmployeeDAO interface

--- 02-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAO.java
+++ 02-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAO.java
@@ -7,4 +7,12 @@ import java.util.List;
 public interface EmployeeDAO {
 
     List<Employee> findAll();
+    
+    Employee findById(int id);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int id);
+
 }
+
